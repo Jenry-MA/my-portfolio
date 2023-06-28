e81d20ea027a0a93297343e2b238995ecd8a5147
JenryUMG <jenrymazariegos@gmail.com>
2023-06-28 11:57:41 -0600

    test custm html tag

---
+++ src/utils/Constants/about.ts
@@ -0,0 +1,38 @@
+const aboutMe = [
+    'Con un enfoque profesional y una actitud positiva, estoy buscando la oportunidad de unirme a un equipo u organizacion con el fin de aportar mi experiencia y conocimientos en el desarrollo de productos innovadores.',
+    'Tengo una amplia experiencia en proyectos de Startups medianas, Turismo orientado en hoteles, Delivery Services, Consumo Masivo, Inventario y Tracking Process.',
+    'Con buenos conocimientos en backe-end con tecnologias como Laravel, Laravel/Livewire, expressJS junto con Cloud Functions de Firebase, al igual con front-end con Laravel Blade, ReactJS, JavaScript, TypeScript, React-Native utilizando frameworks de CSS3 como Bootstrap o TailwindCSS, y gestor de base de datos SQL con ORACLE, MySQL y NoSQL con Firebase Firestore Database.',
+    'Ademas cuento con experiencia trabajando con metologia agiles como Scrum, Diseño de Software, UML.'
+]
+const historyJob = [
+    {
+        title_job: 'Full Stack Freelance',
+        time_job: 'Desde Feb. 2021 a Oct. 2021',
+        description_job: 'Aplicación web de seguimiento de procesos internos de importación y exportación de una naviera.',
+        tecnology_job: 'Tecnologías: Laravel, MySQL, Bootstrap, JavaScript, AWS.'
+        
+    },
+    {
+        title_job: 'Full Stack Freelance',
+        time_job: 'Desde Nov. 2021 a May. 2022',
+        description_job: 'Aplicación web para gestión administrativa de un hotel desde reservaciones con pasarela de pago hasta consumo e inventario.',
+        tecnology_job: 'Tecnologías: Laravel, MySQL, Bootstrap, JavaScript, AWS.'
+        
+    },
+    {
+        title_job: 'Desarrollador Full Stack',
+        time_job: 'Desde Jun. 2022 a Feb. 2023',
+        description_job: 'Responsable de elaboración de soluciones web y colaboración con equipo de trabajo para el desarrollo de soluciones móvil y back-end.',
+        tecnology_job: 'Tecnologías: ReactJS, TailwindCSS, React-Native, TypeScript, JavaScriptm ExpessJS, Firebase y Cloud Functions, Firestore Database NoSQL.'
+        
+    },
+    {
+        title_job: 'Full Stack Freelance',
+        time_job: 'Desde Abr. 2023 - Actualmente en desarrollo',
+        description_job: 'Colaboración con equipo de desarrollo de soluciones web y móvil para la gestión administrativa de una organización de iglesias cristiana.',
+        tecnology_job: 'Tecnologías: ReactJS, TailwindCSS, React-Native, TypeScript, JavaScriptm ExpessJS, Firebase y Cloud Functions, Firestore Database NoSQL.'
+        
+    },
+]
+
+export { aboutMe, historyJob }
--- src/utils/SVGComponents/index.ts
+++ src/utils/SVGComponents/index.ts
@@ -1,2 +1,3 @@
 export { default as Devjm } from './Devjm';
 export { default as Programming} from './Programming';
+export { default as Calendar } from './Calendar'
