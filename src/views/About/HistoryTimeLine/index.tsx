a57fddb4b5b43c298910a8b19194873dac25124c
JenryUMG <jenrymazariegos@gmail.com>
2023-06-29 11:08:31 -0600

    ingreso de textos

--- src/views/About/HistoryTimeLine/index.tsx
+++ src/views/About/HistoryTimeLine/index.tsx
@@ -7,7 +7,9 @@ const HisstoryTimeLine = (): JSX.Element => {
         <div className="z-10 flex items-center justify-center w-10 h-10 bg-indigo-500 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-primary-dark shrink-0">
           <Calendar />
         </div>
+        <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
       </div>
+
       <div className="mt-3 sm:pr-8">
         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
           {data.title_job}
